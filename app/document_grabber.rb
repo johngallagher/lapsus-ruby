e8a562b7a70c7fd5bb10e1444177b63ce87105d9
John Gallagher <john@synapticmishap.co.uk>
2015-01-18 22:33:18 +0000

    Grab urls from Chrome.

--- app/document_grabber.rb
+++ app/document_grabber.rb
@@ -1,18 +1,12 @@
 class DocumentGrabber
   def initialize(bundleIdentifier)
     @bundleIdentifier = bundleIdentifier
-    source = %Q[
-      tell application "System Events"
-        value of attribute "AXDocument" of (front window of the (first process whose bundle identifier is "#{bundleIdentifier}"))
-      end tell
-    ]
-    @script = NSAppleScript.alloc.initWithSource(source)
+    @script = NSAppleScript.alloc.initWithSource(DocumentGrabber.sourceFromBundleIdentifier(bundleIdentifier))
     @script.compileAndReturnError(nil)
   end
 
   def activeDocument
-    Document.new(url: NSURL.URLWithString(grabActiveDocumentUrl), 
-                 bundleIdentifier: @bundleIdentifier)
+    Document.new(url: NSURL.URLWithString(grabActiveDocumentUrl), bundleIdentifier: @bundleIdentifier)
   end
 
   private
@@ -24,6 +18,28 @@ class DocumentGrabber
   rescue => error
     puts "Error: #{error}"
     'missingfile://'
+  end
+
+  def self.sourceFromBundleIdentifier(bundleIdentifier)
+    {
+      'com.google.Chrome' => %Q[
+        tell application "Google Chrome"
+        	tell window 1
+            tell active tab
+              URL
+            end tell
+          end tell
+        end tell
+      ]
+    }.fetch(bundleIdentifier, defaultSourceFromBundleIdentifier(bundleIdentifier))
+  end
+
+  def self.defaultSourceFromBundleIdentifier(bundleIdentifier)
+    %Q[
+        tell application "System Events"
+          value of attribute "AXDocument" of (front window of the (first process whose bundle identifier is "#{bundleIdentifier}"))
+        end tell 
+    ]
   end
 end
 
