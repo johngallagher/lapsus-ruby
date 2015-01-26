5b72fed2c19cc1ce181aaf6c7062c20cfff824f2
John Gallagher <john@synapticmishap.co.uk>
2015-01-26 01:32:30 +0000

    Flattened out the sidebar and added container adding with project population.

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,15 @@
+class Container < CDQManagedObject
+  def container?
+    true
+  end
+
+  def destroy
+    projects.each { |p| p.destroy }
+    super
+  end
+end
 class Project < CDQManagedObject
-  def root
+  def container?
     false
   end
 end
@@ -12,51 +22,70 @@ class Entry < CDQManagedObject
     end
   end
 end
-class RootNode
-  def name
-    'PROJECTS'
-  end
-
-  def root
-    true
-  end
-end
-
-class Node
-  def name
-    'hello'
-  end
-  def root
-    false
-  end
-end
 class MainWindowController < NSWindowController
-  def outlineView(outlineView, child: child, ofItem: item)
-    if child == 0
-      RootNode.new
-    else
-      Node.new
+  include CDQ
+
+  def windowDidLoad
+    @outlineView = window.contentView.viewWithTag(1)
+  end
+
+  def outlineView(outlineView, child: childIndex, ofItem: item)
+    if item.nil?
+      Container.all[childIndex]
+    elsif item.container?
+      item.projects[childIndex]
     end
   end
 
   def outlineView(outlineView, isItemExpandable: item)
-    false
+    item && item.container? && item.projects.count > 0
   end
 
   def outlineView(outlineView, numberOfChildrenOfItem: item)
-    5
+    if item.nil?
+      Container.count
+    elsif item.container?
+      item.projects.count
+    end
   end
 
   def outlineView(outlineView, viewForTableColumn: tableColumn, item: item)
-    if item.root
-      tableCellView = outlineView.makeViewWithIdentifier('HeaderCell', owner: self)
-      tableCellView.textField.textColor = NSColor.headerColor
-      tableCellView.textField.font = NSFont.boldSystemFontOfSize(NSFont.smallSystemFontSize)
-    else
-      tableCellView = outlineView.makeViewWithIdentifier('DataCell', owner: self)
-    end
+    tableCellView = outlineView.makeViewWithIdentifier('DataCell', owner: self)
     tableCellView.textField.stringValue = item.name
     tableCellView
+  end
+
+  def addContainer(object)
+    openDlg = NSOpenPanel.openPanel
+    openDlg.canChooseFiles = false
+    openDlg.canChooseDirectories = true
+    if openDlg.runModal == NSOKButton
+      openDlg.URLs.each do |filename|
+        manager = NSFileManager.defaultManager
+        selectedFolderURL = openDlg.URLs.first
+        urls = manager.contentsOfDirectoryAtURL(selectedFolderURL,
+                                                includingPropertiesForKeys: [NSURLPathKey, NSURLIsDirectoryKey],
+                                                options: 0,
+                                                error: nil)
+        directories = urls.select do |url|
+          directoryPointer = Pointer.new(:object)
+          url.getResourceValue(directoryPointer, forKey: NSURLIsDirectoryKey, error: nil)
+          directoryPointer[0]
+        end
+
+        projects = directories.map do |directory|
+          Project.create(name: directory.lastPathComponent, path: directory.absoluteString)
+        end
+
+        container = Container.create(name: selectedFolderURL.lastPathComponent, path: selectedFolderURL.absoluteString)
+        projects.each do |project|
+          container.projects << project
+        end
+
+        cdq.save
+        @outlineView.reloadData
+      end
+    end
   end
 end
 class PreferencesController < NSWindowController; end
@@ -73,6 +102,9 @@ class AppDelegate
     observeActiveDocumentChanges
     startTrackingDocuments
     showLapsusWindow
+    #c = Container.first
+    #c.projects << Project.first
+    #cdq.save
     true
   end
 
