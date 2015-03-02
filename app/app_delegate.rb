c4bd2957aa8d69aa8f5c6b257f47dc7540b6b047
John Gallagher <john@synapticmishap.co.uk>
2015-03-02 16:24:20 +0000

    Correct no parens back to my prefs.

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,7 +2,7 @@ class AppDelegate
   include CDQ
   attr_accessor :status_menu, :status_item, :tracker, :timer, :moc
 
-  def applicationDidFinishLaunching _notification
+  def applicationDidFinishLaunching(_notification)
     cdq.setup
     true
   end
