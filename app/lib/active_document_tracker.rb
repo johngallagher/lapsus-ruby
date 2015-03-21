da661d650e3177cd145e0f9aeeacc31c879bb928
John Gallagher <john@synapticmishap.co.uk>
2015-03-21 21:11:13 +0000

    Cleaned up code to be more readable.

--- app/lib/active_document_tracker.rb
+++ app/lib/active_document_tracker.rb
@@ -3,25 +3,22 @@ class ActiveDocumentTracker
     @cdq = cdq
     @no_project = Project.create_none
     @active_projects = Project.active.sort_by(:name)
+    @active_entry = Entry.start_for_project(@no_project)
   end
 
   def update
     return if @active_projects.count == 0
 
-    current_project = find_current_project
+    active_project = find_active_project
+    return if @active_entry.project == active_project
 
-    if @active_entry && @active_entry.project != current_project
-      @active_entry.finish
-      @active_entry = Entry.start_for_project(current_project)
-    elsif @active_entry.nil?
-      @active_entry = Entry.start_for_project(current_project)
-    end
+    @active_entry.finish
+    @active_entry = Entry.start_for_project(active_project)
     @cdq.save
   end
 
-  def find_current_project
+  def find_active_project
     url = URIGrabber.grab
-    matching_project = @active_projects.find { |project| url.start_with?(project.urlString) }
-    matching_project || @no_project
+    @active_projects.find(->{ @no_project }){ |project| url.start_with?(project.urlString) }
   end
 end
