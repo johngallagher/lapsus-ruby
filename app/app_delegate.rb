1b55e889d6b9efb4b1aabafecf86a4d1e35bdefb
John Gallagher <john@synapticmishap.co.uk>
2015-03-21 18:44:37 +0000

    Simpler conditional and quits immediately.

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,14 +2,12 @@ class AppDelegate
   include CDQ
 
   def applicationDidFinishLaunching(_notification)
+    return true if RUBYMOTION_ENV == "test"
+
     cdq.setup
-
     @tracker = ActiveDocumentTracker.new(cdq)
-
-    if RUBYMOTION_ENV != "test"
-      @timer = EM.add_periodic_timer 1.0 do
-        @tracker.update
-      end
+    @timer = EM.add_periodic_timer 1.0 do
+      @tracker.update
     end
     true
   end
