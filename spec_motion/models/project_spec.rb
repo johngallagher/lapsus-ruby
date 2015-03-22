2029ff42e5261de56ab22042fa3d6eacc2c49471
John Gallagher <john@synapticmishap.co.uk>
2015-03-22 16:22:33 +0000

    Added tests for idle projects.

--- spec_motion/models/project_spec.rb
+++ spec_motion/models/project_spec.rb
@@ -1,18 +1,35 @@
 describe Project do
+  before do
+    class << self
+      include CDQ
+    end
+
+    cdq.setup
+  end
+
+  after do
+    cdq.reset!
+  end
+
   it "can create a blank project" do
     project = Project.create_none
-    project.urlString.should.nil?
-    project.type.should == 'none'
-    project.name.should == "None"
+    project.attributes.should == { "urlString" => nil, "type" => 'none', "name" => 'None' }
   end
 
   it "can only create one blank project" do
     Project.create_none
     Project.create_none
+
     Project.count.should == 1
-    Project.first.urlString.should.nil?
-    Project.first.type.should == 'none'
-    Project.first.name.should == "None"
+    Project.first.attributes.should == { "urlString" => nil, "type" => 'none', "name" => 'None' }
+  end
+
+  it "creates an idle project" do
+    Project.create_idle
+    Project.create_idle
+
+    Project.count.should == 1
+    Project.first.attributes.should == { "urlString" => nil, "type" => 'idle', "name" => 'Idle' }
   end
 
   it "defaults to a project" do
