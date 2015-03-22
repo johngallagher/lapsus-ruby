ee3d27e5152c5d2e9b1139d14d4cc6cf3151962e
John Gallagher <john@synapticmishap.co.uk>
2015-03-22 16:44:42 +0000

    Reads nicer.

--- app/lib/active_document_tracker.rb
+++ app/lib/active_document_tracker.rb
@@ -4,10 +4,10 @@ class ActiveDocumentTracker
 
   def initialize(cdq, idle_detector = IdleDetector.new)
     @cdq = cdq
-    @no_activity = Activity.find_or_create_none
-    @idle_activity = Activity.find_or_create_idle
+    @none = Activity.find_or_create_none
+    @idle = Activity.find_or_create_idle
     @active_activities = Activity.active
-    @active_entry = Entry.start(@no_activity)
+    @active_entry = Entry.start(@none)
     @idle_detector = idle_detector
     @started_idling_at = nil
   end
@@ -15,27 +15,27 @@ class ActiveDocumentTracker
   def update
     return if @active_activities.count == 0
 
-    active_activity = find_active_activity
+    activity = find_activity
 
-    return if @active_entry.activity == active_activity
+    return if @active_entry.activity == activity
 
     changed_activity_at = Time.now
-    changed_activity_at = Time.now - IDLE_TIME if active_activity == @idle_activity
+    changed_activity_at = Time.now - IDLE_TIME if activity == @idle
 
     @active_entry.finish(changed_activity_at)
-    @active_entry = Entry.start(active_activity, changed_activity_at)
+    @active_entry = Entry.start(activity, changed_activity_at)
     @cdq.save
   end
 
-  def find_active_activity
+  def find_activity
     @started_idling_at = Time.now if @idle_detector.idle? && !@started_idling_at
 
     if @idle_detector.idle? && @started_idling_at + IDLE_TIME <= Time.now
       @started_idling_at = nil
-      return @idle_activity
+      return @idle
     else
       url = URIGrabber.grab
-      @active_activities.find(->{ @no_activity }){ |activity| url.start_with?(activity.urlString) }
+      @active_activities.find(->{ @none }){ |activity| url.start_with?(activity.urlString) }
     end
   end
 end
