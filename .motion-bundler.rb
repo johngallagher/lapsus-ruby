d65937132d2e58a52faa2ab815250fa3a7823cbe
John Gallagher <john@synapticmishap.co.uk>
2015-04-12 19:07:47 +0100

    Added a basic ui with a list of entries.

--- .motion-bundler.rb
+++ .motion-bundler.rb
@@ -1,8 +1,9 @@
 module MotionBundler
   FILES = [
     "/Users/johngallagher/Dropbox/Projects/ProgrammingProjects/CurrentProjects/lapsus/.motion-bundler.rb",
-    "/Users/johngallagher/.gem/ruby/2.2.0/gems/motion-bundler-0.2.1/lib/motion-bundler/device/boot.rb",
-    "/Users/johngallagher/.gem/ruby/2.2.0/gems/motion-bundler-0.2.1/lib/motion-bundler/device/core_ext.rb",
+    "/Users/johngallagher/.gem/ruby/2.2.0/gems/motion-bundler-0.2.1/lib/motion-bundler/simulator/boot.rb",
+    "/Users/johngallagher/.gem/ruby/2.2.0/gems/motion-bundler-0.2.1/lib/motion-bundler/simulator/core_ext.rb",
+    "/Users/johngallagher/.gem/ruby/2.2.0/gems/motion-bundler-0.2.1/lib/motion-bundler/simulator/console.rb",
     "/Users/johngallagher/.gem/ruby/2.2.0/gems/bubble-wrap-1.7.1/motion/shortcut.rb",
     "/Users/johngallagher/.gem/ruby/2.2.0/gems/bubble-wrap-1.7.1/lib/bubble-wrap/version.rb",
     "/Users/johngallagher/.gem/ruby/2.2.0/gems/bubble-wrap-1.7.1/motion/util/deprecated.rb",
@@ -43,6 +44,7 @@ module MotionBundler
     "/Users/johngallagher/Dropbox/Projects/ProgrammingProjects/CurrentProjects/lapsus/mocks/uri/common.rb",
     "/Users/johngallagher/Dropbox/Projects/ProgrammingProjects/CurrentProjects/lapsus/mocks/uri/generic.rb",
     "/Users/johngallagher/Dropbox/Projects/ProgrammingProjects/CurrentProjects/lapsus/app/app_delegate.rb",
+    "/Users/johngallagher/Dropbox/Projects/ProgrammingProjects/CurrentProjects/lapsus/app/controllers/main_window_controller.rb",
     "/Users/johngallagher/Dropbox/Projects/ProgrammingProjects/CurrentProjects/lapsus/app/lib/active_document_tracker.rb",
     "/Users/johngallagher/Dropbox/Projects/ProgrammingProjects/CurrentProjects/lapsus/app/lib/idle_detector.rb",
     "/Users/johngallagher/Dropbox/Projects/ProgrammingProjects/CurrentProjects/lapsus/app/lib/uri_grabber.rb",
@@ -50,16 +52,18 @@ module MotionBundler
     "/Users/johngallagher/Dropbox/Projects/ProgrammingProjects/CurrentProjects/lapsus/app/models/entry.rb"
   ]
   FILES_DEPENDENCIES = {
-    "/Users/johngallagher/.gem/ruby/2.2.0/gems/motion-bundler-0.2.1/lib/motion-bundler/device/boot.rb" => [
-      "/Users/johngallagher/.gem/ruby/2.2.0/gems/motion-bundler-0.2.1/lib/motion-bundler/device/core_ext.rb"
+    "/Users/johngallagher/.gem/ruby/2.2.0/gems/motion-bundler-0.2.1/lib/motion-bundler/simulator/boot.rb" => [
+      "/Users/johngallagher/.gem/ruby/2.2.0/gems/motion-bundler-0.2.1/lib/motion-bundler/simulator/core_ext.rb",
+      "/Users/johngallagher/.gem/ruby/2.2.0/gems/motion-bundler-0.2.1/lib/motion-bundler/simulator/console.rb"
     ],
     "/Users/johngallagher/Dropbox/Projects/ProgrammingProjects/CurrentProjects/lapsus/mocks/time.rb" => [
       "/Users/johngallagher/Dropbox/Projects/ProgrammingProjects/CurrentProjects/lapsus/mocks/date/format.rb"
     ]
   }
   REQUIRES = [
-    "/Users/johngallagher/.gem/ruby/2.2.0/gems/motion-bundler-0.2.1/lib/motion-bundler/device/boot.rb",
+    "/Users/johngallagher/.gem/ruby/2.2.0/gems/motion-bundler-0.2.1/lib/motion-bundler/simulator/boot.rb",
     "/Users/johngallagher/Dropbox/Projects/ProgrammingProjects/CurrentProjects/lapsus/.motion-bundler.rb",
+    "console",
     "core_ext",
     "date/format",
     "ostruct",
--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,14 +1,22 @@
 class AppDelegate
   include CDQ
 
+  attr_reader :main_context
+
   def applicationDidFinishLaunching(_notification)
     return true if RUBYMOTION_ENV == "test"
 
     cdq.setup
+    @main_context = cdq.contexts.current
+
     @tracker = ActiveDocumentTracker.new(cdq)
     @timer = EM.add_periodic_timer 1.0 do
       @tracker.update
     end
+
+    @main_window_controller = MainWindowController.alloc.initWithWindowNibName('MainWindow')
+    @main_window_controller.window.makeKeyAndOrderFront(self)
+
     true
   end
 
