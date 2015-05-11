58ba8c7db43ec1ff7bf924cafc2808cb0ebb26cf
John Gallagher <john@synapticmishap.co.uk>
2015-05-11 20:46:52 +0100

    Remove old code as we don't have an array controller that uses it now.

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,13 +1,11 @@
 class AppDelegate
   include CDQ
 
-  attr_reader :main_context
 
   def applicationDidFinishLaunching(_)
     return true if RUBYMOTION_ENV == "test"
 
     cdq.setup
-    @main_context = cdq.contexts.current
 
     @tracker = ActiveDocumentTracker.new(cdq)
     @timer = EM.add_periodic_timer 1.0 do
