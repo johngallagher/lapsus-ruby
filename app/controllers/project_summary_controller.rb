726cc8d0261605159329b052ac96ed5f23e20790
John Gallagher <john@synapticmishap.co.uk>
2015-05-11 21:28:00 +0100

    Set no owner so awakeFromNib doesn't get called a zillion times.

--- app/controllers/project_summary_controller.rb
+++ app/controllers/project_summary_controller.rb
@@ -26,12 +26,12 @@ class ProjectSummaryController < NSViewController
   def tableView(tableView, viewForTableColumn: column, row: row)
     project = @projects[row]
     if column.identifier == "ProjectSummaryTimeColumn"
-      cell = tableView.makeViewWithIdentifier("ProjectTime", owner: self)
+      cell = tableView.makeViewWithIdentifier("ProjectTime", owner: nil)
       #cell.textField.formatter = MinutesDurationFormatter.new
       #cell.textField.objectValue = project.time_for(selected_date_range)
       cell.textField.stringValue = project.time_for(selected_date_range)
     else
-      cell = tableView.makeViewWithIdentifier("ProjectIconAndName", owner: self)
+      cell = tableView.makeViewWithIdentifier("ProjectIconAndName", owner: nil)
       cell.textField.stringValue = project.name
     end
     cell
